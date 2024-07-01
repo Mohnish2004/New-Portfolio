f12b89e57eecfd7ac9a5efd41c089afc1fa38b41
Mohnish <mohnishgopi@gmail.com>
2024-06-30 23:15:43 -0400

    goddamn this one slash

--- app/page.tsx
+++ app/page.tsx
@@ -492,7 +492,7 @@ const Home = () => {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href=""
+            href={"/"}
           >
             <ArrowIcon />
             <p className="ml-2 h-7">Learn more </p>
